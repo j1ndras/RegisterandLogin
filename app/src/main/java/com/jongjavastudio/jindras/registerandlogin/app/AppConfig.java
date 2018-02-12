d45306c4366f4f9e900583ff05460b8f67f8cbd6
JINDRAS <jonyindrasono.uyp@gmail.com>
2018-02-13 06:06:23 +0700

    Initial commit

---
+++ app/src/main/java/com/jongjavastudio/jindras/registerandlogin/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.jongjavastudio.jindras.registerandlogin.app;
+
+/**
+ * Created by JINDRAS on 11/02/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://assiasoft.com/indekos/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://assiasoft.com/indekos/register.php";
+}
